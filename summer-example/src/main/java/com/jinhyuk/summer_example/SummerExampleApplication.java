70c4f8e2ec5373ed75704e83ff2de73b4b73c037
Jinhyuk Kim <reniowood@gmail.com>
2018-09-29 13:54:59 +0900

    Fix example application

--- summer-example/src/main/java/com/jinhyuk/summer_example/SummerExampleApplication.java
+++ summer-example/src/main/java/com/jinhyuk/summer_example/SummerExampleApplication.java
@@ -2,14 +2,15 @@ package com.jinhyuk.summer_example;
 
 import com.jinhuyk.summer_core.SummerMainApplication;
 import com.jinhuyk.summer_core.annotations.SummerApplication;
+import com.jinhuyk.summer_core.context.ApplicationContext;
 import com.jinhyuk.summer_example.components.ComponentA;
 
 @SummerApplication
 public class SummerExampleApplication {
     public static void main(String[] args) {
-        SummerMainApplication.run(SummerExampleApplication.class);
+        ApplicationContext applicationContext = SummerMainApplication.run(SummerExampleApplication.class);
 
-        ComponentA componentA = (ComponentA) SummerMainApplication.getComponent("ComponentA");
+        ComponentA componentA = applicationContext.getComponent(ComponentA.class);
 
         System.out.println(componentA.name());
     }
